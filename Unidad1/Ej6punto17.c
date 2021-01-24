dfe7dca1a74b8fca71dd9e04b1f926ca19c2a612
Manuela <manuela@pop-os.localdomain>
2021-01-23 23:45:27 -0500

    Agrego punto 17 del ejercicio 6 guia2

---
+++ Unidad1/Ej6punto17.c
@@ -0,0 +1,33 @@
+//programa usando funciones fscanf, fprintf, fgetc, fputc
+//lee numeros (por linea) desde un archivo, calcula la suma y la escribe
+#include <stdio.h>
+#include <stdlib.h>
+
+char nombreArchivo [50] = "/home/manuela/Escritorio/archivo2.txt";
+int main(void){
+    FILE *archivo; //declara archivo
+    char datos[10];
+    int suma;
+    //abrir archivo para lectura
+    archivo = fopen(nombreArchivo,"r");
+    //valida que no sea nulo
+    if(archivo== NULL){
+        printf("No se pudo abrir archivo \n%s", nombreArchivo);
+        exit(EXIT_FAILURE);
+    }
+    //recorrer
+    while(fgets(datos,10,archivo)){ //lee cada linea
+        suma = suma + atoi(datos); //acumula y castea cadena a entero
+    }
+    fprintf(archivo, "%d\n", suma);
+    fscanf(archivo, "%d", &suma);
+    fclose(archivo);  //cerrar archivo
+    //abrir archivo para escritura
+    archivo = fopen(nombreArchivo,"w");
+    fprintf(archivo,"Suma de los numeros= %d\n ", suma); //escribe el valor de la suma en el archivo
+    fputs("suma realizada con exito\n",archivo);
+    //cerrar archivo
+    fclose(archivo);
+    printf("%s","Se procesó y escribió archivo");
+    return 0;
+}
