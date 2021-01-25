a786790cf2b57cde82f2628178e47233df466e5d
Manuela <manuela@pop-os.localdomain>
2021-01-25 12:25:39 -0500

    agrego ejercicio 9

---
+++ Unidad1/Ej9.c
@@ -0,0 +1,16 @@
+//analizar funcionamiento del programa (retorno de scanf)
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    char residuo[10];
+    printf("enter a number: ");
+    while (scanf("%d", &a) != 1)
+    {
+        // input was not a number, ask again:
+        scanf("%s",&residuo); //sacar del flujo de entrada la cadena que no ha sido convertida
+        printf("enter a number:");
+    }
+    printf("You entered %d.\n", a);
+}
