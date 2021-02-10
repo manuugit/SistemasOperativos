e1f6011f1fafecd833dbf8ed1f0018dfc92510f0
Manuela <manuela@pop-os.localdomain>
2021-02-10 13:31:24 -0500

    avance proyecto 1 carga archivo + validacion

--- Unidad1/proyecto1.c
+++ Unidad1/proyecto1.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+//estructuras de datos
 typedef struct estudiante{
     int cedula;
     char nombre[32];
@@ -15,65 +16,85 @@ typedef struct bd{
     estudiante_t *registroEstudiante;
 } bd_t;
 
+//prototipado de funciones
 bd_t mkdb(char[32], int);
-void loaddb(char[32], char[32]);
+void loaddb(char[32]);
+void savedb(char[32]);
+
 estudiante_t est;
 estudiante_t *pest = &est;
 bd_t bd;
 bd_t *pbd = &bd;
 bd_t *buffer;
+int existebd=0;
 
 int main(void){
     mkdb("based",3);
-    loaddb("based","/home/manuela/SistemasOperativos/Unidad1/estudiantes.txt");
+    loaddb("/home/manuela/SistemasOperativos/Unidad1/estudiantes.txt");
     return 0;
 }
 
 //funciones
  bd_t mkdb(char nombrebd [32], int sizebd){
     bd.tamaño = sizebd;
+    existebd=1;
     return bd;
 }
 
-void loaddb(char nombrebd [32], char archivo[32]){
+void loaddb(char archivo[32]){
     FILE *archivof; //declara archivo
     char datos[100];
     int linea=0;
     int control;
-    //abrir archivo para lectura
-    archivof = fopen(archivo,"r");
-    //valida que no sea nulo
-    if(archivof== NULL){
+
+    if(existebd ==1){
+        //abrir archivo para lectura
+        archivof = fopen(archivo,"r");
+        //valida que no sea nulo
+        if(archivof== NULL){
         printf("No se pudo abrir archivo \n%s", archivo);
         exit(EXIT_FAILURE);
+        }
+        //reserva de memoria
+        pbd ->registroEstudiante = (estudiante_t *)malloc(sizeof(estudiante_t)*bd.tamaño);
+        linea = 0;
+        //recorrer
+        while(fgets(datos,sizeof(datos),archivof)!=NULL){ //lee cada linea
+                control++;
+                if(control ==1){
+                    pbd->registroEstudiante[linea].cedula= atoi(datos);
+                    //printf("%d\n",pbd->registroEstudiante[linea].cedula);
+                }
+                if(control == 2){
+                    strcpy(pbd->registroEstudiante[linea].nombre,datos);
+                    //printf("%s\n",pbd->registroEstudiante[linea].nombre);
+                }
+                if(control == 3){
+                    pbd->registroEstudiante[linea].semestre = atoi(datos);
+                    //printf("%d\n",pbd->registroEstudiante[linea].semestre);
+                    control=0;
+                    linea++;
+                }
+        }
+        for (int i=0; i<bd.tamaño; i++){
+          printf("%d  %s %d\n",pbd->registroEstudiante[i].cedula,pbd->registroEstudiante[i].nombre,pbd->registroEstudiante[i].semestre);
+        }
+        fclose(archivof);  //cerrar archivo
+        printf("%s\n","La base de datos fue cargada correctamente");
+        }
+
+        else{
+            printf("%s\n","La base de datos no se ha creado");
+        }
+    
+}
+
+void savedb(char archivoSalida[32]){
+    //abrir archivo para escritura
+    /*FILE *archivoS =fopen(archivoSalida,"w");
+    if (archivoSalida == NULL) {
+        perror("Error: ");
+        return EXIT_FAILURE;
     }
-    //reserva de memoria
-    buffer = (bd_t *)malloc(sizeof(est)*bd.tamaño);
-    linea = 0;
-    //recorrer
-    while(fgets(datos,sizeof(datos),archivof)!=NULL){ //lee cada linea
-            control++;
-            if(control ==1){
-                (pest+linea)->cedula= atoi(datos);
-                //printf("%d\n",(pest+linea)->cedula);
-            }
-            if(control == 2){
-                strcpy((pest+linea)->nombre,datos);
-                //printf("%s\n",(pest+linea)->nombre);
-            }
-            if(control == 3){
-                (pest+linea)->semestre = atoi(datos);
-                //printf("%d\n",(pest+linea)->semestre);
-                control=0;
-                linea++;
-            }
-        pbd ->registroEstudiante = (pest+linea);
-    }
-    for (int i=0; i<bd.tamaño; i++){
-       // printf("%d %d %s %d\n",i,(pest+i)->cedula,(pest+i)->nombre,(pest+i)->semestre);
-        printf("%d,%s %d",(pbd->registroEstudiante+i)->cedula,(pbd->registroEstudiante+i)->nombre,(pbd->registroEstudiante+i)->semestre);
-    }
-    buffer = pbd;
-    fclose(archivof);  //cerrar archivo
-    printf("%s\n","Se procesó y escribió archivo");
+    for (int i=0; i)*/
 }
