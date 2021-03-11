636840646f0099ba0a751f6cf2e9db4df8254a37
Manuela <manuela@pop-os.localdomain>
2021-03-10 23:07:12 -0500

    proyecto2 hilo2

--- Unidad2/proyecto2/prog.c
+++ Unidad2/proyecto2/prog.c
@@ -71,6 +71,7 @@ int main(void){
         }
 
         //se imprime en pantalla desde la estructura de datos
+        printf("Archivo original\n");
         for (int i=0; i<archivoComp.cantidadlineas;i++){
             printf("%s %s %d\n",(archivoComp.plinea+i)->nombre, (archivoComp.plinea+i)->ocupacion, (archivoComp.plinea+i)->edad);
 
@@ -83,28 +84,70 @@ int main(void){
     
     fclose(archivoEntrada);
 
-    pthread_create(&hilo1, NULL, &ordenarInverso, "archivoEp2.txt");
-    pthread_create(&hilo2, NULL, &ordenarAlfabeticamente, "archivoEp2.txt");
+    pthread_create(&hilo1, NULL, &ordenarInverso, "salida1.txt");
+    pthread_create(&hilo2, NULL, &ordenarAlfabeticamente, "salida2.txt");
     pthread_join(hilo1,NULL);
     pthread_join(hilo2, NULL);
     return 0;
 }
 
 void *ordenarInverso(void *archivo){
-    archivoEntrada = fopen(archivo, "r");
     //archivo para escritura
-    FILE *archivoSalida= fopen("salida1.txt", "w");
-    for (int i=archivoComp.cantidadlineas; i>=0;i--){
+    FILE *archivoSalida= fopen(archivo, "w");
+    for (int i=archivoComp.cantidadlineas-1; i>=0;i--){
             fprintf(archivoSalida, "%s %s %d\n",(archivoComp.plinea+i)->nombre, (archivoComp.plinea+i)->ocupacion, (archivoComp.plinea+i)->edad);
         }
 
-    fclose(archivoEntrada);
     fclose(archivoSalida);
 
     return 0;
 }
 
 void *ordenarAlfabeticamente(void *archivo){
+    //archivo para escritura
+    FILE *archivoSalida= fopen(archivo, "w");
+    //vectores auxiliares
+    char ocupaciones[archivoComp.cantidadlineas][20];
+    char ocupacionesO[archivoComp.cantidadlineas][20];
+    char min [20] =  "zz";
+    int pos;
+    int control=0;
 
+    for(int i=0; i<archivoComp.cantidadlineas; i++){
+        strcpy(ocupaciones[i],(archivoComp.plinea+i)->ocupacion);
+    }
+
+    for (int i=0; i<archivoComp.cantidadlineas; i++){
+        //calculo la palabra menor
+        for(int j=0; j<archivoComp.cantidadlineas;j++){
+            if (strcmp(ocupaciones[j], min) <0){
+                strcpy(min, ocupaciones[j]); //guardo la palabra menor
+                pos = j;
+            }   
+        }
+        strcpy(ocupaciones[pos],"zz"); //elimino la palabra menor actual
+        strcpy(ocupacionesO[i],min); //guardo la palabra minima
+        strcpy(min,"zz");
+    }
+    
+    //guardo en el archivo de salida, en el orden correspondiente
+    for (int i=0; i<archivoComp.cantidadlineas;i++){
+        for(int j=0; j<archivoComp.cantidadlineas;j++){
+            if (strcmp(ocupacionesO[i],(archivoComp.plinea+j)->ocupacion)==0){
+                fprintf(archivoSalida,"%s %s %d\n",(archivoComp.plinea+j)->nombre, (archivoComp.plinea+j)->ocupacion, (archivoComp.plinea+j)->edad);
+                control++;
+                 if(control ==archivoComp.cantidadlineas ){
+                     break;
+                 }
+                break;
+            }
+
+        if(control ==archivoComp.cantidadlineas ){
+            break;
+        }
+        }   
+    }
+
+    fclose(archivoSalida);
     return 0;
 }
