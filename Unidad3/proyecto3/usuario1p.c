8ce58b0d21bdada596dd5cf4024ab283424df12b
Manuela <manuela.hernandezr@upb.edu.co>
2021-04-18 17:31:36 -0500

    agrego proyecto 3

--- Unidad3/proyecto3/usuario1p.c
+++ Unidad3/proyecto3/usuario1p.c
@@ -8,18 +8,18 @@
 #include <fcntl.h>           /* For O_* constants */
 #include <unistd.h>
 #include <pthread.h>
+#include <semaphore.h>       //for semaphores
 #include <sys/wait.h>
-#include <semaphore.h>
 
 #define SH_SIZE1 200
 #define SH_SIZE2 200
-#define SNAME "sem1"
 
 //prototipado de funciones
 void *enviarMensaje(void*);
 void *recibirMensaje(void*);
-sem_t *sem1;
-sem_t *sem2;
+
+sem_t *sem1w, *sem1r;
+sem_t *sem2w, *sem2r;
 
 int main(void) {
    
@@ -27,6 +27,37 @@ int main(void) {
     int shm_fd2;
     pthread_t hiloEnvia;
     pthread_t hiloRecibe;
+
+    //crear semaforos
+    mode_t perms = 0666;
+    int flags = O_CREAT;
+    unsigned int value = 0; //inicia cerrado
+
+    sem1w = sem_open("sem1w", flags, perms, value);
+    if(sem1w == SEM_FAILED){
+        perror("user 1 sem_open sem1w fails: ");
+        exit(EXIT_FAILURE);
+    }
+
+    sem1r = sem_open("sem1r", flags, perms, value);
+    if(sem1r == SEM_FAILED){
+        perror("user 1 sem_open sem1r fails: ");
+        exit(EXIT_FAILURE);
+    }
+
+    //abrir semaforos del proceso 2
+    sem2w = sem_open("sem2w",0);
+    if(sem2w == SEM_FAILED){
+        perror("user1 sem_open sem2w: ");
+        exit(EXIT_FAILURE);
+    }
+
+    sem2r = sem_open("sem2r",0);
+    if(sem2r == SEM_FAILED){
+        perror("user1 sem_open sem2r: ");
+        exit(EXIT_FAILURE);
+    }
+
 
     //crea memoria compartida
     shm_fd1 = shm_open("MemoryObject1", O_CREAT | O_RDWR, 0600);
@@ -40,8 +71,6 @@ int main(void) {
         perror("Truncation failed shm1: ");
         exit(EXIT_FAILURE);
     }
-
-    fprintf(stdout, "The memory region is truncated.\n");
 
     //abre memoria 2
     shm_fd2 = shm_open("MemoryObject2", O_RDONLY, 0600);
@@ -63,17 +92,22 @@ int main(void) {
 
 void *enviarMensaje(void *shm_fd){
     int len;
-    char buf[40];
+    char buf[SH_SIZE1];
     char* p;
     void* map;
 
     printf("Estado: listo para enviar o recibir mensajes.\n");
-    printf("Ingrese un mensaje, escriba ^D para terminar:\n");
+    printf("Ingrese un mensaje, escriba ^C para terminar:\n");
 
     while(fgets(buf, sizeof buf, stdin) != NULL){
-
         len = strlen(buf);
         if (buf[len-1] == '\n') buf[len-1] = '\0';
+
+        //abrir candado semaforo escritura 1
+        if(sem_post(sem1w) == -1){
+            perror("sem_post error sem1w: ");
+            exit(EXIT_FAILURE);
+        }
 
         map = mmap(NULL, SH_SIZE1, PROT_WRITE, MAP_SHARED, shm_fd, 0);
         if (map == MAP_FAILED) {
@@ -81,36 +115,66 @@ void *enviarMensaje(void *shm_fd){
             exit(EXIT_FAILURE);
         }
         p= (char*)map;
-        strcpy(p,buf);
-        printf("P=%s",p);
-
-        fprintf(stdout, "Data is written to the shared memory.\n");
+        memcpy(p, buf, sizeof(buf));
 
         if (munmap(p, SH_SIZE1) < 0) {
             perror("Unmapping failed: ");
             exit(EXIT_FAILURE);
-     }
+        }
+
+        //cerrar candado semaforo lectura 2
+        if(sem_wait(sem2r) == -1){
+            perror("sem_wait error sem2r: ");
+            exit(EXIT_FAILURE);
+        } 
+    }
+
+    //cerrar semaforos
+    if(sem_close(sem1w) == -1){
+        perror("sem_close fails sem1w: ");
+        exit(EXIT_FAILURE);
+    }
+
+    if(sem_unlink("sem1w") == -1){
+        perror("sem_unlink falls sem1w: ");
+        exit(EXIT_FAILURE);
+    }
+
+    if(sem_close(sem1r) == -1){
+        perror("sem_close fails sem1r: ");
+        exit(EXIT_FAILURE);
+    }
+
+    if(sem_unlink("sem1r") == -1){
+        perror("sem_unlink falls sem1r: ");
+        exit(EXIT_FAILURE);
     }
 
     
-     if (close(shm_fd) < 0) {
-         perror("Closing shm failed: ");
-         exit(EXIT_FAILURE);
-     }
+    //cerrar espacio de memoria
+    if (close(shm_fd) < 0) {
+        perror("Closing shm failed: ");
+        exit(EXIT_FAILURE);
+    }
 
-     if (shm_unlink("MemoryObject1") < 0) {
-         perror("Unlink failed: ");
-         exit(EXIT_FAILURE);
-     }
+    if (shm_unlink("MemoryObject1") < 0) {
+        perror("Unlink failed: ");
+        exit(EXIT_FAILURE);
+    }
 
     return 0;
 
 }
 void *recibirMensaje(void *shm_fd){
-    int fd;
     char *p;
 
     for(;;) { 
+
+        //cerrar candado semaforo escritura 2
+        if(sem_wait(sem2w) == -1){
+            perror("sem_wait error sem2w: ");
+            exit(EXIT_FAILURE);
+        } 
 
         void* map = mmap(NULL, SH_SIZE2, PROT_READ, MAP_SHARED, shm_fd, 0);
         if (map == MAP_FAILED) {
@@ -119,14 +183,20 @@ void *recibirMensaje(void *shm_fd){
         }
         p = (char*)map;
 
-        fprintf(stdout, "The contents of shared memory object: %s\n", p);
+        fprintf(stdout, "Usuario 2: %s\n", p);
 
         //desmapear
         if (munmap(p, SH_SIZE2) < 0) {
             perror("Unmapping failed: ");
             exit(EXIT_FAILURE);
         }
-        
+
+        //abrir candado semaforo lectura 1
+        if(sem_post(sem1r) == -1){
+            perror("sem_post error sem1r: ");
+            exit(EXIT_FAILURE);
+        }
+
     } 
 
     printf("Los mensajes han finalizado.\n");
--- Unidad3/proyecto3/usuario2p.c
+++ Unidad3/proyecto3/usuario2p.c
@@ -8,18 +8,18 @@
 #include <fcntl.h>           /* For O_* constants */
 #include <unistd.h>
 #include <pthread.h>
+#include <semaphore.h>       //for semaphores
 #include <sys/wait.h>
-#include <semaphore.h>
 
 #define SH_SIZE1 200
 #define SH_SIZE2 200
-#define SNAME "sem1"
 
 //prototipado de funciones
 void *enviarMensaje(void*);
 void *recibirMensaje(void*);
-sem_t *sem1;
-sem_t *sem2;
+
+sem_t *sem1w, *sem1r;
+sem_t *sem2w, *sem2r;
 
 int main(void) {
    
@@ -27,6 +27,36 @@ int main(void) {
     int shm_fd1;
     pthread_t hiloEnvia;
     pthread_t hiloRecibe;
+
+    //crear semaforos
+    mode_t perms = 0666;
+    int flags = O_CREAT;
+    unsigned int value = 0; //inicia cerrado
+
+    sem2w = sem_open("sem2w", flags, perms, value);
+    if(sem2w == SEM_FAILED){
+        perror("user 2 sem_open sem2w fails: ");
+        exit(EXIT_FAILURE);
+    }
+
+    sem2r = sem_open("sem2r", flags, perms, value);
+    if(sem2r == SEM_FAILED){
+        perror("user 2 sem_open sem2r fails: ");
+        exit(EXIT_FAILURE);
+    }
+
+    //abrir semaforos del proceso 1
+    sem1w = sem_open("sem1w",0);
+    if(sem1w == SEM_FAILED){
+        perror("user2 sem_open sem1w: ");
+        exit(EXIT_FAILURE);
+    }
+
+    sem1r = sem_open("sem1r",0);
+    if(sem1r == SEM_FAILED){
+        perror("user2 sem_open sem1r: ");
+        exit(EXIT_FAILURE);
+    }
 
     //crea memoria compartida
     shm_fd2 = shm_open("MemoryObject2", O_CREAT | O_RDWR, 0600);
@@ -40,8 +70,6 @@ int main(void) {
         perror("Truncation failed shm2: ");
         exit(EXIT_FAILURE);
     }
-
-    fprintf(stdout, "The memory region is truncated.\n");
 
     //abre memoria 1
     shm_fd1 = shm_open("MemoryObject1", O_RDONLY, 0600);
@@ -63,17 +91,23 @@ int main(void) {
 
 void *enviarMensaje(void *shm_fd){
     int len;
-    char buf[40];
+    char buf[SH_SIZE2];
     char* p;
     void* map;
 
     printf("Estado: listo para enviar o recibir mensajes.\n");
-    printf("Ingrese un mensaje, escriba ^D para terminar:\n");
+    printf("Ingrese un mensaje, escriba ^C para terminar:\n");
 
     while(fgets(buf, sizeof buf, stdin) != NULL){
 
         len = strlen(buf);
         if (buf[len-1] == '\n') buf[len-1] = '\0';
+
+        //abrir candado semaforo de escritura 2
+        if(sem_post(sem2w) == -1){
+            perror("sem_post error sem2w: ");
+            exit(EXIT_FAILURE);
+        }
 
         map = mmap(NULL, SH_SIZE2, PROT_WRITE, MAP_SHARED, shm_fd, 0);
         if (map == MAP_FAILED) {
@@ -81,16 +115,41 @@ void *enviarMensaje(void *shm_fd){
             exit(EXIT_FAILURE);
         }
         p= (char*)map;
-        strcpy(p,buf);
-        printf("P=%s",p);
-
-        fprintf(stdout, "Data is written to the shared memory.\n");
+        memcpy(p, buf, sizeof(buf));
 
         //desmapear
         if (munmap(p, SH_SIZE2) < 0) {
             perror("Unmapping failed: ");
             exit(EXIT_FAILURE);
         }
+
+        //cerrar candado semaforo de lectura 1
+        if(sem_wait(sem1r) == -1){
+            perror("sem_wait error sem1r: ");
+            exit(EXIT_FAILURE);
+        } 
+
+    }
+
+    //cerrar semaforos
+    if(sem_close(sem2w) == -1){
+        perror("sem_close fails sem2w: ");
+        exit(EXIT_FAILURE);
+    }
+
+    if(sem_unlink("sem2w") == -1){
+        perror("sem_unlink falls sem2w: ");
+        exit(EXIT_FAILURE);
+    }
+
+    if(sem_close(sem2r) == -1){
+        perror("sem_close fails sem2r: ");
+        exit(EXIT_FAILURE);
+    }
+
+    if(sem_unlink("sem2r") == -1){
+        perror("sem_unlink falls sem2r: ");
+        exit(EXIT_FAILURE);
     }
 
     //cerrar espacio de memoria
@@ -109,8 +168,13 @@ void *enviarMensaje(void *shm_fd){
 }
 void *recibirMensaje(void *shm_fd){
     char *p;
-
     for(;;) { 
+
+        //cerrar candado semaforo de escritura 1
+        if(sem_wait(sem1w) == -1){
+            perror("sem_wait error sem1w: ");
+            exit(EXIT_FAILURE);
+        } 
 
         void* map = mmap(NULL, SH_SIZE1, PROT_READ, MAP_SHARED, shm_fd, 0);
         if (map == MAP_FAILED) {
@@ -119,14 +183,19 @@ void *recibirMensaje(void *shm_fd){
         }
         p = (char*)map;
 
-        fprintf(stdout, "The contents of shared memory object: %s\n", p);
+        fprintf(stdout, "Usuario 1: %s\n", p);
 
         //desmapear
         if (munmap(p, SH_SIZE1) < 0) {
             perror("Unmapping failed: ");
             exit(EXIT_FAILURE);
         }
-        
+
+        //abrir candado semaforo de lectura 2
+        if(sem_post(sem2r) == -1){
+            perror("sem_post error sem2r: ");
+            exit(EXIT_FAILURE);
+        }
     } 
 
     printf("Los mensajes han finalizado.\n");
