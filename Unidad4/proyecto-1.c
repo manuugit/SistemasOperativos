8bbed07ce692cd697c1e123af586e5ecd2d9afcf
Manuela <manuela.hernandezr@upb.edu.co>
2021-05-01 18:36:29 -0500

    proyecto 1 v1

---
+++ Unidad4/proyecto-1.c
@@ -0,0 +1,98 @@
+//leer el contenido de un directorio y de sus subdirectorios
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+#include <string.h>
+
+char tipoRetornado [32];
+
+// Prototipado de funciones
+void identificarTipo(char ruta [512]);
+void listarDirectorio(char ruta [512]);
+
+void unix_error(char *msg) /* Unix-style error */
+{
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+    exit(0);
+}
+
+int main(int argc, char **argv)
+{
+    char tipo [32];
+    char ruta [512];
+    strcpy(ruta,"../");
+
+    if (argc != 2) {
+        printf("usage: %s <pathname>\n", argv[0]);
+        exit(1);
+    }
+
+    identificarTipo(ruta);
+    strcpy(tipo,tipoRetornado);
+        
+    if (strcmp(tipo, "directory")!=0){
+        printf("No es posible recorrer el directorio, porque la ruta no corresponde a un directorio");
+    }
+    else{
+        listarDirectorio(ruta);
+    }
+    
+    return(0);
+
+
+}
+
+void identificarTipo(char ruta [512]){
+    struct stat stat_str;
+    stat(ruta, &stat_str);
+    
+    if (S_ISREG(stat_str.st_mode))     /* Determine file type */
+	  strcpy(tipoRetornado,"regular");
+    else if (S_ISDIR(stat_str.st_mode))
+	  strcpy(tipoRetornado,"directory");
+    else 
+	  strcpy(tipoRetornado,"other");
+}
+
+void listarDirectorio(char ruta [512]){
+    DIR *streamp; 
+    struct dirent *dep; 
+    int control =0;
+    char nuevaRuta [512];
+    char tipo [32];
+
+    streamp = opendir(ruta);
+    errno = 0;
+    
+    while ((dep = readdir(streamp)) != NULL) {
+        if(strcmp(dep->d_name, "..")!=0 && strcmp(dep->d_name, ".")!=0){
+            strcpy(nuevaRuta, ruta);
+            printf("%s\n", nuevaRuta);
+            strcat(nuevaRuta, dep->d_name);
+            printf("%s\n", nuevaRuta);
+           
+            identificarTipo(nuevaRuta);
+            strcpy(tipo,tipoRetornado);
+
+            if (strcmp(tipo, "directory")==0){
+                printf("Found file: %s\n", dep->d_name);
+                printf("rutaA %s\n",nuevaRuta);
+                strcat(nuevaRuta, "/");
+                listarDirectorio(nuevaRuta);
+                printf("ruta %s\n",nuevaRuta);
+            }
+            else {
+                printf("Found file %s\n",dep->d_name);
+            }        
+        } 
+    }
+
+    if (errno != 0)
+        unix_error("readdir error");
+
+    closedir(streamp);
+}
