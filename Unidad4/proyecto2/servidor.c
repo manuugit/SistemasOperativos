e0a33e4baf33a1cbcef8e33af8c34a97a33a679f
Manuela <manuela.hernandezr@upb.edu.co>
2021-05-07 19:57:07 -0500

    proyecto 2 v2

--- Unidad4/proyecto2/servidor.c
+++ Unidad4/proyecto2/servidor.c
@@ -7,7 +7,6 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <signal.h>
-
 #define PORT 6666
 #define BUF_SIZE 128
 
@@ -49,7 +48,7 @@ int main(int argc, char *argv[]){
         perror("Socket creation fails\n");
         exit(EXIT_FAILURE);
     }
-    printf("Socket created\n");
+    //printf("Socket created\n");
 
      // 3. turn off bind address checking
     status = setsockopt(server_sd, SOL_SOCKET, SO_REUSEADDR,(int *) &enable, sizeof(enable));
@@ -87,7 +86,7 @@ int main(int argc, char *argv[]){
         (client+i)->conectado = 0;
     }
 
-    printf("Server listening\n");
+    printf("Servidor escuchando\n");
     status = pthread_create(&hiloComandos, NULL, &recibirComandos, NULL);
     if(-1 == status){
         perror("Pthread read fails: ");
@@ -97,10 +96,10 @@ int main(int argc, char *argv[]){
 
     while(1){
          // 6. Accept:
-        printf("Waiting for a client\n");
+        printf("Esperando por clientes\n");
         client_sd = accept(server_sd, NULL, NULL);
 
-        printf("Client connected\n");
+        //printf("Client connected\n");
         if(-1 == client_sd){
             perror("Accept fails: ");
             close(server_sd);
@@ -111,27 +110,28 @@ int main(int argc, char *argv[]){
         (client+cont)->rxState = 1;
         (client+cont)->conectado = 1;
         (client+cont)->conteoCliente = cont;
-        printf("Num %d\n",(client+cont)->conteoCliente);
 
-        printf("Create Pthread for reading\n");
+        //printf("Create Pthread for reading\n");
         if(cont<10){
             status = pthread_create(&rxThreadId[cont],NULL,&readThread,(client+cont));
+            cont++;
         }
         else{
             printf("Ya se excedio el limite de usuarios conectados");
-            close((client+cont)->socket);
+            close(client_sd);
+            //close((client+cont)->socket);
         }
-        cont++;
+        
             
         if(-1 == status){
             perror("Pthread read fails: ");
             close(server_sd);
             exit(EXIT_FAILURE);
 
-        /*if(0 == client.rxState){
+        if(0 == client->rxState){
                 printf("Client closed the socket\n");
                 break;
-            }*/
+            }
         }
 
 
@@ -152,6 +152,7 @@ int main(int argc, char *argv[]){
        
     }
 
+    free(client);
     exit(EXIT_SUCCESS);
 }
 
@@ -161,8 +162,9 @@ int main(int argc, char *argv[]){
     int status;
     char buf[BUF_SIZE];
     int control;
+    int encontro=0;
 
-    while(strcmp(comando,"exitP")!=0){
+    while(strcmp(comando,"exit")!=0){
         printf("\n%s","Escriba el comando\n show para listar los clientes conectados\n -d nombre para desconectar un cliente\n");
         scanf("%s",comando);
         if (strcmp(comando,"-d")==0){
@@ -175,11 +177,13 @@ int main(int argc, char *argv[]){
                     (client+i)->rxState = 0;
                     cont--;
                     control=i;
+                    encontro=1;
                     break;
                 }
             }
 
-            for(int i=0; i<=maxClientes; i++){
+            if(encontro==1){
+                for(int i=0; i<=maxClientes; i++){
                 if((client+i)->conectado == 1 && (client+i)->conteoCliente !=control ){
                     strcpy(buf, (client+control)->nombreCliente);
                     strcat(buf, " ha abandonado el chat\n");
@@ -189,16 +193,22 @@ int main(int argc, char *argv[]){
                         break;
                     }
                 }
-                if((client+i)->conteoCliente !=control){
+                if((client+i)->conteoCliente == control){
                     strcpy(buf, "Te has desconectado\n");
                     status = write((client+control)->socket,buf, strlen(buf)+1);
                     close((client+control)->socket);
-                    if(-1 == status){
-                        perror("Server write to client fails: ");
-                        break;
+                        if(-1 == status){
+                            perror("Server write to client fails: ");
+                            break;
+                        }
                     }
                 }
             }
+            else{
+                printf("Usuario no encontrado\n");
+            }
+
+            encontro=0;
 
             /*for(int i=control; i<=maxClientes; i++){
                 (client+i)->conteoCliente = i;
@@ -207,14 +217,27 @@ int main(int argc, char *argv[]){
             
         }
         else if (strcmp(comando,"show")==0){
-            printf("conteo:%d\n",cont);
             //mostrar clientes 
-            for(int i=0; i<maxClientes; i++){
-                if((client+i)->conectado !=0){
-                    printf("%s\n",(client+i)->nombreCliente);
-                }
-               
+            if(cont ==0){
+                printf("No hay clientes conectados\n");
             }
+            else{
+                printf("CLIENTES CONECTADOS\n");
+                for(int i=0; i<maxClientes; i++){
+                    if((client+i)->conectado !=0){
+                        printf("%s\n",(client+i)->nombreCliente);
+                    }   
+                }   
+            }
+            printf("conteo:%d\n",cont);
+           
+        }
+        else if(strcmp(comando,"exit")==0){
+            printf("\n");
+            break;
+        }
+        else{
+            printf("Comando no valido\n");
         }
      }
     return 0;
@@ -225,12 +248,26 @@ void * readThread(void *arg){
     ssize_t numOfBytes;
     char buf[BUF_SIZE];
     int status;
+    char bvd[BUF_SIZE];
 
     numOfBytes = read(clienteE->socket, buf, BUF_SIZE);
-    printf("cont=%d/n",cont);
+    //printf("cont=%d/n",cont);
     strcpy((client+(cont-1))->nombreCliente,buf);
-    printf("Primer mensaje: %s\n",buf);
+    //printf("Primer mensaje: %s\n",buf);
 
+    strcpy(bvd,buf);
+    strcat(bvd, " se ha unido al chat");
+    for(int i=0; i<=maxClientes; i++){
+        if ((client+i)->conectado == 1){
+            status = write((client+i)->socket, bvd, strlen(bvd)+1);
+            if(-1 == status){
+                perror("Server write to client fails: ");
+                break;
+            }       
+        }
+    }
+
+    
     while(1){
         numOfBytes = read(clienteE->socket, buf, BUF_SIZE);
         if(0 == numOfBytes){
@@ -257,8 +294,9 @@ void * readThread(void *arg){
             }
         }
     }
-    printf("Terminate Pthread for reading\n");
+    //printf("Terminate Pthread for reading\n");
     clienteE->rxState = 0;
     close(clienteE->socket);
+
     return NULL;
     }
